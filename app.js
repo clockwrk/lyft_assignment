4a5fd3158d3af78ab0c2d27443ca78c7b81c5574
Jose Renteria <JoseRenteria.sf@outlook.com>
2018-01-25 07:28:23 -0800

    fixed dynamic port mapping

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const PORT = process.env || 3000
+const PORT = process.env.PORT || 3000
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
